41a0e4fc2b6355531916e86c811e6d716abb589c
Deborah Chu <deborahchu@Deborahs-Air.attlocal.net>
2024-08-14 01:35:29 -0700

    simple package manager with circular dependency checks and dependency conflict resolution

--- src/NpmRegistryClient.ts
+++ src/NpmRegistryClient.ts
@@ -1,12 +1,53 @@
 import fs from "fs";
 import https from "https";
 import path from "path";
-import tar from "tar";
+import tar from "tar-fs";
+import gunzip from "gunzip-maybe";
 
 /**
  * A client for the NPM registry API.
  */
 export class NpmRegistryClient {
+
+  async getLatestPackageVersion(name: string): Promise<any> {
+    const resp = await fetch(
+      `https://registry.npmjs.org/${name}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await resp.json();
+    return data['dist-tags'].latest;
+  }
+
+  // This function isn't used anywhere yet, but would be used to find dependency versions compatible with ^ notation. 
+  // ex. The noop3 dependency (^13.7.2) in the is-thirteen package would be resolved to version 13.8.1 instead of 13.7.2.
+  // I chose to download the 13.7.2 version specified by is-thirteen for now but noticed npm would download 13.8.1 so
+  // I suspect this kind of functionality should be used when downloading dependencies. 
+  async getLatestCompatibleVersion(name: string, version: string): Promise<any> {
+    const resp = await fetch(
+      `https://registry.npmjs.org/${name}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await resp.json();
+    const versions = Object.keys(data.versions);
+    const majorVersion = version.split(".")[0];
+    for (const v of versions.reverse()) {
+      if (v.split(".")[0] == majorVersion) {
+        return v;
+      }
+    }
+    return "Version Not Found";
+  }
+
   /**
    * Request package information from the NPM registry API as described [here](https://github.com/npm/registry/blob/main/docs/REGISTRY-API.md#getpackageversion)
    *
@@ -59,19 +100,17 @@ export class NpmRegistryClient {
 
     // Extract the tarball
     const targetDir = path.dirname(downloadToPath);
-    try {
-      await tar.extract({
-        file: downloadToPath,
-        cwd: targetDir,
-      });
-    } catch (e) {
-      console.error(`Error extracting package ${name}@${absoluteVersion}:`, e);
-      return;
-    } finally {
-      // Delete the tarball
-      fs.unlinkSync(downloadToPath);
-    }
-
-    fs.renameSync(path.join(targetDir, "package"), downloadToPath);
+    const fileStream = fs.createReadStream(downloadToPath);
+    fileStream.pipe(gunzip()).pipe(tar.extract(targetDir))
+      .on("finish", () => {
+        fs.unlinkSync(downloadToPath); // delete tar file
+        fs.renameSync(path.join(targetDir, "package"), `${downloadToPath}-${absoluteVersion}`); // rename downloaded package
+        console.log(`Downloaded package to ${downloadToPath}-${absoluteVersion}`)
+      })
+      .on("error",(e) => {
+        fileStream.close();
+        console.error(`Error extracting package ${name}@${absoluteVersion}:`, e);
+        fs.unlinkSync(downloadToPath); // delete tar file if extraction fails
+      })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 import { program } from "commander";
+import { NpmRegistryClient } from "./NpmRegistryClient";
+import fs from 'fs';
+import fsExtra from 'fs-extra';
+import { IDependencyMap } from "package-json-type";
+import { Queue } from 'queue-typescript';
+
+var registryClient: NpmRegistryClient = new NpmRegistryClient();
+
+// Takes in two version numbers and returns the latest of the two.
+function getLatestVersion(v1 : string, v2 : string) : string {
+  const [major1, minor1, patch1] = v1.split(".");
+  const [major2, minor2, patch2] = v2.split(".");
+  [major1, minor1, patch1, major2, minor2, patch2].forEach((x) => Number(x));
+
+  if (major1 > major2) { return v1; }
+  if (major1 < major2) { return v2; }
+  if (minor1 > minor2) { return v1; }
+  if (minor1 < minor2) { return v2; }
+  if (patch1 > patch2) { return v1; }
+  return v2
+}
 
 /**
  * Adds the dependency to the “dependencies” object in package.json
@@ -8,9 +29,16 @@ import { program } from "commander";
 program
   .command("add <package>")
   .description("Add a package")
-  .action((pkg) => {
-    // -- IMPLEMENT ADD COMMAND -- //
-    const [packageName, version] = pkg.split("@");
+  .action(async (pkg) => {
+    let [packageName, version] = pkg.split("@");
+    if (version === undefined) {
+      version = await registryClient.getLatestPackageVersion(packageName)
+    }
+
+    // add package to dependencies in output/package.json
+    let packageJson = JSON.parse(fs.readFileSync('./output/package.json', 'utf8'));
+    packageJson.dependencies[packageName] = version;
+    fs.writeFileSync('./output/package.json', JSON.stringify(packageJson, null, 2));
   });
 
 /**
@@ -22,7 +50,49 @@ program
   .command("install")
   .description("Install dependencies")
   .action(async () => {
-    // -- IMPLEMENT INSTALL COMMAND -- //
+    let packageJson = JSON.parse(fs.readFileSync('./output/package.json', 'utf8'));
+    let initialDependencies : IDependencyMap = packageJson.dependencies;
+    let allDependencies: IDependencyMap = {};
+
+    // initialize list of dependencies to check
+    let checkDependencies = new Queue<Array<string>>();
+    for (const [packageName, version] of Object.entries(initialDependencies)) {
+      checkDependencies.enqueue([packageName, version])
+    }
+
+    // go through dependencies
+    while (checkDependencies.length != 0) {
+      let dep = checkDependencies.dequeue();
+      const [packageName, version] = [dep[0], dep[1]];
+      // if come across a circular dependency or a dependency with another version, choose the latest dependency version for simplicity
+      if (allDependencies[packageName] !== undefined) {
+        if (version == allDependencies[packageName]) { continue; } // circular
+        let latest = getLatestVersion(version, allDependencies[packageName]);
+        console.log(`Dependency conflict for package ${packageName}: Installing version ${latest}`)
+        if (version != latest) {
+          continue;
+        }
+      }
+      // add dependency to allDependencies 
+      allDependencies[packageName] = version;
+      // add dependency's dependencies to checkDependencies
+      let packageInfo = await registryClient.getPackageInfo(packageName, version);
+      let packageDependencies : IDependencyMap = packageInfo.dependencies;
+      if (packageDependencies) {
+        for (const [n, v] of Object.entries(packageDependencies)) {
+          checkDependencies.enqueue([n, v.replace("^", "")])
+        }
+      }
+    }
+
+    // creates output/node_modules folder or clears it if it exists
+    fsExtra.ensureDirSync('./output/node_modules/');
+    fsExtra.emptyDirSync('./output/node_modules/');
+
+    // download all dependencies to output/node_modules
+    for (const [packageName, version] of Object.entries(allDependencies)) {
+      await registryClient.downloadTarball(packageName, version, `./output/node_modules/${packageName}`);
+    }
   });
 
 program.parse(process.argv);
